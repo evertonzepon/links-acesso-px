3d021dfe6694846389a162c35e2756e3e7d1722b
Everton Zepon <110439259+evertonzepon@users.noreply.github.com>
2025-01-11 13:40:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.getElementById('darkModeToggle').addEventListener('click', () => {
     const isDarkMode = document.body.classList.contains('dark-mode');
     document.getElementById('darkModeToggle').textContent = isDarkMode ? '☀️' : '🌙';
 });
+
+// Obtém todos os cards
+const cards = document.querySelectorAll('.link-card');
+
+// Adiciona o evento de clique em cada card
+cards.forEach(card => {
+  card.addEventListener('click', function() {
+    // Obtém o link do atributo 'data-url' e redireciona
+    const url = card.getAttribute('data-url');
+    window.open(url, '_blank'); // Abre o link em uma nova aba
+  });
+});
+
