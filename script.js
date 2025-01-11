107b3237ac9e15df5637c1e833e382e8323f78e3
Everton Zepon <110439259+evertonzepon@users.noreply.github.com>
2025-01-11 12:17:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('darkModeToggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    document.getElementById('darkModeToggle').textContent = isDarkMode ? '☀️' : '🌙';
+});
